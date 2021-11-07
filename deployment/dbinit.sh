1db547f1da0af3cf3494d9ce888f7fafdb801652
Mingwei Zhang <mingwei@mwzhang.com>
2021-11-06 17:11:21 -0700

    consolidate database credentials into one file
    this makes changing credentials much easier. also avoid accidental
    partial changes which might cause issues during deployment.

--- deployment/dbinit.sh
+++ deployment/dbinit.sh
@@ -6,7 +6,7 @@ echo "downloading bootstrap database file"
 curl https://spaces.bgpkit.org/broker/broker-database-dump.gz -o /tmp/data.gz --silent
 
 echo "bootstraping data"
-gunzip < /tmp/data.gz | PGPASSWORD=broker psql --username "broker_user" --dbname "broker" > /dev/null
+gunzip < /tmp/data.gz | PGPASSWORD=${POSTGRES_PASSWORD} psql --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" > /dev/null
 
 echo "clean up temporary file"
 rm /tmp/data.gz
