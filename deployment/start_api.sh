8a6eca189109ed417f94bffa01d56015e4dc8415
Mingwei Zhang <mingwei@mwzhang.com>
2022-04-26 07:38:35 -0700

    docker deployment fixes and readme update

--- deployment/start_api.sh
+++ deployment/start_api.sh
@@ -3,5 +3,6 @@
 export BROKER_PG_HOST=postgres
 export BROKER_PG_USER=${POSTGRES_USER}
 export BROKER_PG_DB=${POSTGRES_DB}
+export BROKER_PG_PASSWORD=${POSTGRES_PASSWORD}
 
-/usr/local/bin/uvicorn --app-dir /usr/local/bin/ broker-api:app --host 0.0.0.0 --port 18888 --root-path '/v2'
+/usr/local/bin/uvicorn --app-dir /usr/local/bin/ broker-api:app --host 0.0.0.0 --port 18888
