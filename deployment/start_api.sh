9c0aec3096d462f587b6d14f99f1371e31cb4166
Mingwei Zhang <mingwei@mwzhang.com>
2022-04-25 18:06:13 -0700

    update start_api script

---
+++ deployment/start_api.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export BROKER_PG_HOST=postgres
+export BROKER_PG_USER=${POSTGRES_USER}
+export BROKER_PG_DB=${POSTGRES_DB}
+
+/usr/local/bin/uvicorn --app-dir /usr/local/bin/ broker-api:app --host 0.0.0.0 --port 18888 --root-path '/v2'
