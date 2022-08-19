d8fb0972bd762b2ec61ae76a953eff3c6a4ea295
Mingwei Zhang <mingwei@mwzhang.com>
2022-08-19 16:19:20 -0700

    allow Kafka config be passed in with env var

--- deployment/update.sh
+++ deployment/update.sh
@@ -2,5 +2,11 @@
 
 set -e
 
-RUST_LOG=bgpkit_broker_backend /usr/local/bin/bgpkit-broker-updater -c /usr/local/etc/bgpkit-broker-collectors.conf -l -d postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB} 2>/tmp/bgpkit-broker-updater.log
+if [[ -z "${KAFKA_BROKER}" ]]; then
+  KAFKA_OPTS="--kafka-broker ${KAFKA_BROKER} --kafka-topic ${KAFKA_TOPIC}"
+else
+  KAFKA_OPTS=""
+fi
+
+RUST_LOG=bgpkit_broker_backend /usr/local/bin/bgpkit-broker-updater -c /usr/local/etc/bgpkit-broker-collectors.conf -l -d postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB} ${KAFKA_OPTS} 2>/tmp/bgpkit-broker-updater.log
 PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c "REFRESH MATERIALIZED VIEW latest_times" > /dev/null
