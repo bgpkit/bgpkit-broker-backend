9558a7fc8ebf110b7177fd5ef7a9322331819c60
Mingwei Zhang <mingwei@mwzhang.com>
2022-08-22 07:07:33 -0700

    fix kafka opts configuration

--- deployment/update.sh
+++ deployment/update.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-if [[ -z "${KAFKA_BROKER}" ]]; then
+if [[ ! -z "${KAFKA_BROKER}" ]]; then
   KAFKA_OPTS="--kafka-broker ${KAFKA_BROKER} --kafka-topic ${KAFKA_TOPIC}"
 else
   KAFKA_OPTS=""
 fi
 
-RUST_LOG=bgpkit_broker_backend /usr/local/bin/bgpkit-broker-updater -c /usr/local/etc/bgpkit-broker-collectors.conf -l -d postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB} ${KAFKA_OPTS} 2>/tmp/bgpkit-broker-updater.log
-PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c "REFRESH MATERIALIZED VIEW latest_times" > /dev/null
+RUST_LOG=bgpkit_broker_backend /usr/local/bin/bgpkit-broker-updater -c /usr/local/etc/bgpkit-broker-collectors.conf -l -d ${DATABASE_URL} ${KAFKA_OPTS} 2>/tmp/bgpkit-broker-updater.log
+psql ${DATABASE_URL} -c "REFRESH MATERIALIZED VIEW latest_times" > /dev/null
